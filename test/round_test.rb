d3b59d0975e35d9c758d6816366cf7684ae820e6
Cassie Achzenick <caachzenick@gmail.com>
2019-11-20 19:20:43 -0700

    Add test it guess can be correct

--- test/round_test.rb
+++ test/round_test.rb
@@ -81,7 +81,7 @@ class RoundTest < Minitest::Test
     assert_instance_of Turn, round.take_turn('Juneau')
   end
 
-  def test_new_turns_class_is_turn
+  def test_it_is_the_turn_class
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -91,5 +91,17 @@ class RoundTest < Minitest::Test
     new_turn = round.take_turn("Juneau")
 
     assert_equal Turn, new_turn.class
+  end
+
+  def test_it_can_be_correct
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal true ,new_turn.correct?
   end
 end
