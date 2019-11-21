3f20c11d9a273cc385694b4484ea681258d6813f
Cassie Achzenick <caachzenick@gmail.com>
2019-11-20 18:26:54 -0700

    Create round file & add test to see if it has 3 cards available

---
+++ test/round_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+class RoundTest < Minitest::Test
+
+  def test_it_has_3_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    assert_instance_of Card, card_1
+    assert_instance_of Card, card_2
+    assert_instance_of Card, card_3
+  end
+end
