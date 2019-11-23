36932132cb1a4b7f028466c41e7173aa20b5a2cc
Cassie Achzenick <caachzenick@gmail.com>
2019-11-22 19:36:14 -0700

    Add and pass test that feedback can be given for a wrong answer

--- test/round_test.rb
+++ test/round_test.rb
@@ -194,4 +194,16 @@ class RoundTest < Minitest::Test
     assert_equal 2, round.turns.count
   end
 
+  def test_feedback_can_be_given_for_wrong_answer
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn_alaska = round.take_turn("Juneau")
+    new_turn_viking = round.take_turn("Venus")
+
+    assert_equal "Incorrect.", round.turns.last.feedback
+  end
+
 end
