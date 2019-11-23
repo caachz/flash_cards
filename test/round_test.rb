24294041cf70637efce4fa0a16009e5c16d6e6be
Cassie Achzenick <caachzenick@gmail.com>
2019-11-22 19:34:09 -0700

    Add and pass test that turn counts a new turn after a new turn is created

--- test/round_test.rb
+++ test/round_test.rb
@@ -182,4 +182,16 @@ class RoundTest < Minitest::Test
     assert_instance_of Turn, round.take_turn("Venus")
   end
 
+  def test_turns_counts_with_addition_of_a_new_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn_alaska = round.take_turn("Juneau")
+    new_turn_viking = round.take_turn("Venus")
+
+    assert_equal 2, round.turns.count
+  end
+
 end
