5ac88ee86111fd9b038401da6a810be583dd1f44
Cassie Achzenick <caachzenick@gmail.com>
2019-11-23 16:04:29 -0700

    Add criteria to runner file and add start method to round not yet working

---
+++ lib/flashcard_runner.rb
@@ -0,0 +1,19 @@
+require './lib/card.rb'
+require './lib/deck.rb'
+require './lib/turn.rb'
+require './lib/round.rb'
+
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+deck = Deck.new([card_1, card_2, card_3])
+round = Round.new(deck)
+
+# def start
+# puts "Welcome! You're playing with #{deck.length} cards.\n
+# -------------------------------------------------\n
+# This is card number #{round.number_of_turns+1}\n
+# Question: #{round.deck.cards[0].question}\n
+# Answer here => #{gets.chomp}"
+# end
+round.start
--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,6 @@
 class Round
-  attr_reader :deck, :turns, :total_correct_by_category, :number_correct, :number_of_turns, :correct_cards
+  attr_reader :deck, :turns, :total_correct_by_category, :number_correct,
+  :number_of_turns, :correct_cards, :current_turn
 
   def initialize(deck)
     @deck = deck
@@ -8,6 +9,7 @@ class Round
     @correct_cards = []
     @total_correct_by_category = 0
     @number_correct = 0
+    @current_turn = current_turn
   end
 
   def current_card
@@ -15,13 +17,13 @@ class Round
   end
 
   def take_turn(guess)
-    current_turn = Turn.new(guess, current_card)
-    @turns << current_turn
+    @current_turn = Turn.new(guess, current_card)
+    @turns << @current_turn
     @number_of_turns += 1
-    if current_turn.correct?
-      @correct_cards << current_turn
+    if @current_turn.correct?
+      @correct_cards << @current_turn
     end
-    current_turn
+    @current_turn
   end
 
   def number_correct
@@ -57,4 +59,15 @@ class Round
     end
     total_correct_category.to_f/total_category_answered.to_f*100
   end
+
+  def start
+  puts "Welcome! You're playing with #{@deck.cards.length} cards.\n
+  -------------------------------------------------\n
+  This is card number #{@number_of_turns+1}\n
+  Question: #{deck.cards[number_of_turns].question}\n
+  Answer => #{answer = gets.chomp}"
+  puts answer
+
+  end
+
 end
