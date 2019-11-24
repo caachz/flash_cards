8b9956b426e11702c38c699521be882108bb82f5
Cassie Achzenick <caachzenick@gmail.com>
2019-11-23 19:50:37 -0700

    Take out \n from all strings

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -7,7 +7,7 @@ class CardGenerator
 
   def cards
    File.open(@filename).each do |line|
-     line_split = line.split(',')
+     line_split = line.chomp("\n").split(',')
      @array_of_cards_as_strings << line_split
    end
     p @array_of_cards_as_strings
