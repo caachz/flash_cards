737f9d0a61d101c0cef10d2389e36ccce4a3d07c
Cassie Achzenick <caachzenick@gmail.com>
2019-11-24 07:32:30 -0700

    Finish flash card iteration 4

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -2,12 +2,13 @@ require './lib/card.rb'
 require './lib/deck.rb'
 require './lib/turn.rb'
 require './lib/round.rb'
+require './lib/card_generator'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-deck = Deck.new([card_1, card_2, card_3])
-round = Round.new(deck)
+# card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+# card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+# deck = Deck.new([card_1, card_2, card_3])
+round = Round.new
 
 # def start
 # puts "Welcome! You're playing with #{deck.length} cards.\n
